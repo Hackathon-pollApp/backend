fb6173b3ad2fadb216d9680913940a7c859edbce
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-30 12:41:08 +0200

    domain.Option done!

---
+++ src/main/java/poll/domain/Option.java
@@ -0,0 +1,63 @@
+package poll.domain;
+
+import poll.utilities.InvalidParamException;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+
+@Entity(name = "OptionQuestion")
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "optionId", updatable = false, nullable = false)
+    private Integer id;
+    private Integer eventId;
+    private String optionText;
+    private Integer quantity;
+
+    public Option() {
+    }
+
+    public Option(Integer eventId, String option, Integer quantity) throws InvalidParamException {
+        checkValidStringField(option);
+        this.eventId = eventId;
+        this.optionText = option;
+        this.quantity = quantity;
+    }
+
+    private void checkValidStringField(String string) throws InvalidParamException {
+        if (string.equals(""))
+            throw new InvalidParamException();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Integer eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getOption() {
+        return optionText;
+    }
+
+    public void setOption(String option) throws InvalidParamException {
+        checkValidStringField(option);
+        this.optionText = option;
+    }
+
+    public Integer getQuantity() {
+
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
